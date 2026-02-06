1babd55463a2694a371fd3a0b0068fdf5fdbe431
fu-stacker <Fuaddev5@gmail.com>
2026-02-06 05:16:16 +0300

    added some codes

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ console.log(__dirname);
 console.log(__filename);
 
 // creating own global variable with node js
-global.myname = "fuad";
-console.log(myname);
+global.mynamee = "fuad";
+console.log(mynamee);
 
+const hello = require("./hello.js");
 // modules 1
-const sayhi = require("./hello.js");
-sayhi("fuad");
-sayhi("abdurehman");
-// modules 2
-const add = require("./hello.js");
-console.log(add(1, 2));
+
+// sayhi("fuad");
+// sayhi("abdurehman");
+// // modules 2
+
+// console.log(add(1, 2));
+
+hello.sayhi("ms abebe");
+hello.sayhi(hello.myname);
+console.log(hello.sum(9, 0));
+hello.modulehi();
+// simple use of object
+let { sayhi, sum, modulehi, myname } = hello;
+sayhi("ms abebe");
+sayhi(myname);
+console.log(sum(9, 0));
+modulehi();
--- hello.js
+++ hello.js
@@ -1,12 +1,25 @@
+function modul() {
+  console.log("hello every one this is mudules course part 1.");
+}
+
 function sayhi(name) {
   console.log(`hello ${name}`);
 }
 
-module.exports = sayhi;
+// module.exports = sayhi;
 
 function add(a, b) {
   return a + b;
 }
-module.exports = add;
 
-// in the above code i write 2 function and export 2 times
+// module.exports = add;
+
+// in the above code i write 2 function and export 2 times and this will no longer use full and
+// happens error so lets ecport by object
+
+module.exports = {
+  myname: "fuad",
+  sayhi: sayhi,
+  sum: add,
+  modulehi: modul,
+};
